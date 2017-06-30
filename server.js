29c4c80456f7e9ac61bf80b37c19e0dbc43ba06f
nancy <nancy@nancys-MacBook-Air.local>
2017-06-30 15:05:44 -0600

    mod

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const path = require('path');
 const port = process.env.PORT || 8080;  //process.env.PORT is and environment variable given by heroku
 const app = express();
 
+app.listen(port, function(){
+    console.log('Server started ' + port);
+});
+
+
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-app.listen(port);
-console.log('Server started');
+
 
 
 /* to run this app locally, run webpack -p to create bundle.js, then node server.js*/
